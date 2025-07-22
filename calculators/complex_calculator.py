f4a1862f9e62ed6c68b0742fd016b5a0e0a81ed8
Louis Billaut <ragedfear4875@gmail.com>
2025-07-23 05:49:45 +0800

    add basic complex calculator

---
+++ calculators/complex_calculator.py
@@ -0,0 +1,49 @@
+from complex.complex_generator import complex # Overwrite complex module
+
+class Complex_Calculator():
+    def __init__(self):
+        pass
+    
+    def sum(self, a: complex, b: complex):
+        '''
+        Returns the sum of two complex numbers
+        '''
+        re_sum = a.Re() + b.Re()
+        im_sum = a.Im() + b.Im()
+        return complex(real = re_sum, imaginary = im_sum)
+    
+    def difference(self, a: complex, b: complex):
+        '''
+        Returns the difference of two complex numbers
+        '''
+        re_diff = a.Re() - b.Re()
+        im_diff = a.Im() - b.Im()
+        return complex(real = re_diff, imaginary = im_diff)
+    
+    def product(self, a: complex, b: complex):
+        '''
+        Returns the product of two complex numbers
+        '''
+        re_prod = a.Re()*b.Re() - a.Im()*b.Im()
+        im_prod = a.Re()*b.Im() + a.Im()*b.Re()
+        return complex(real = re_prod, imaginary= im_prod)
+    
+    def quotient(self, a: complex, b: complex):
+        '''
+        Returns the quotient of two complex numbers
+        '''
+        if not (b.Re() or b.Im()):
+            raise ZeroDivisionError(f"{a} cannot be divided by {b}")
+
+        re_quot = (a.Re()*b.Re() + a.Im()*b.Im()) / (b.Re()**2 + b.Im()**2)
+        im_quot = (a.Im()*b.Re() - a.Re()*b.Im()) / (b.Re()**2 + b.Im()**2)
+        return complex(real = re_quot, imaginary= im_quot)
+    
+calc = Complex_Calculator()
+a = complex(real = 4, imaginary = 3)
+b = complex(real = -0.5, imaginary = -6.5)
+
+print(calc.sum(a,b))
+print(calc.difference(a,b))
+print(calc.product(a,b))
+print(calc.quotient(a,b))
