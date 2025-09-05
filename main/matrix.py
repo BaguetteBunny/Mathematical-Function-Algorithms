226e76fa8a0e16989b3a731da9b5c1ba57d9987f
Louis Billaut <ragedfear4875@gmail.com>
2025-09-05 23:56:34 +0800

    add matrix addition

--- main/matrix.py
+++ main/matrix.py
@@ -7,6 +7,10 @@ class matrix():
         for row in self.matrix:
             assert len(row) == self.row_length
 
+    def __iter__(self):
+        for row in self.matrix:
+            yield row
+
     def __eq__(self, other):
         assert isinstance(other, matrix)
         for index, row in enumerate(other):
@@ -15,9 +19,18 @@ class matrix():
 
         return True
     
-    def __iter__(self):
-        for row in self.matrix:
-            yield row
+    def __add__(self, other):
+        assert isinstance(other, matrix)
+        assert self.row_length == other.row_length
+        assert self.col_length == other.col_length
+
+        new_matrix_list = []
+        for row_index, row in enumerate(other):
+            new_matrix_row_list = []
+            for col_index, col in enumerate(row):
+                new_matrix_row_list.append(self.matrix[row_index][col_index] + col)
+            new_matrix_list.append(new_matrix_row_list)
+        return matrix(new_matrix_list)
 
     def __repr__(self):
         final_string = ""
@@ -29,4 +42,4 @@ class matrix():
 
 A = matrix([[1, 2, 3], [4, 5, 6]])
 B = matrix([[1, 2, 3], [4, 5, 6]])
-print(A == B)
+print(A + B)
