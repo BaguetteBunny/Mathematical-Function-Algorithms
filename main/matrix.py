cd7a8dc3fc5fef95167e9fd667e1847ab8505790
Louis Billaut <ragedfear4875@gmail.com>
2025-09-06 00:10:40 +0800

    add hidden methods for error detection

--- main/matrix.py
+++ main/matrix.py
@@ -11,18 +11,17 @@ class matrix():
         for row in self.matrix:
             yield row
 
-    def __eq__(self, other):
-        assert isinstance(other, matrix)
+    def __eq__(self, other: 'matrix'):
+        self.__check_other_type_at_operation(other, '==')
         for index, row in enumerate(other):
             if self.matrix[index] == row: continue
             else: return False
 
         return True
     
-    def __add__(self, other):
-        assert isinstance(other, matrix)
-        assert self.row_length == other.row_length
-        assert self.col_length == other.col_length
+    def __add__(self, other: 'matrix'):
+        self.__check_other_type_at_operation(other, '+')
+        self.__check_other_equal_row_col(other)
 
         new_matrix_list = []
         for row_index, row in enumerate(other):
@@ -31,13 +30,32 @@ class matrix():
                 new_matrix_row_list.append(self.matrix[row_index][col_index] + col)
             new_matrix_list.append(new_matrix_row_list)
         return matrix(new_matrix_list)
-
+    
     def __repr__(self):
         final_string = ""
         for row in self.matrix:
             final_string += f"{row}\n"
 
         return final_string
+    
+    def __check_other_type_at_operation(self, other: 'matrix', operator: str):
+        try:
+            assert isinstance(other, matrix)
+        except:
+            raise TypeError(f"Unsupported operand type(s) for {operator}: 'matrix' and '{type(other)}'")
+        
+    def __check_other_equal_row_col(self, other: 'matrix', check_row: bool = True, check_col: bool = True):
+        if check_row:
+            try:
+                assert self.row_length == other.row_length
+            except:
+                raise ValueError("Matrices must have the same row length.")
+            
+        if check_col:
+            try:
+                assert self.col_length == other.col_length
+            except:
+                raise ValueError("Matrices must have the same column length.")
 
 
 A = matrix([[1, 2, 3], [4, 5, 6]])
