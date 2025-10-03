bec7c4170803e95d9d9d15c641d7caeeae3ad348
Louis Billaut <ragedfear4875@gmail.com>
2025-10-03 22:33:14 +0800

    implement other basic operators

--- main/vec3.py
+++ main/vec3.py
@@ -4,7 +4,6 @@ class Vec3():
         self.y = y
         self.z = z
         self.vector = (x, y, z)
-        self.__inv_vector = (1/x, 1/y, 1/z)
 
     def __isNullVector(self):
         return self.x == self.y == self.z == 0
@@ -21,35 +20,66 @@ class Vec3():
     def __abs__(self):
         return self.norm()
     
-    def _add__(self, other):
+    def __add__(self, other):
         if isinstance(other, (int, float)):
             return Vec3(self.x + other, self.y + other, self.z + other)
-            
-        elif isinstance(other, 'Vec3'):
+        elif isinstance(other, Vec3):
             return Vec3(self.x + other.x, self.y + other.y, self.z + other.z)
-            
         return NotImplemented
             
     def __radd__(self, other):
-        return self.__add__(other)
+        if isinstance(other, (int, float)):
+            return Vec3(self.x + other, self.y + other, self.z + other)
+        return NotImplemented
         
     def __sub__(self, other):
         if isinstance(other, (int, float)):
             return Vec3(self.x - other, self.y - other, self.z - other)
-            
         elif isinstance(other, Vec3):
             return Vec3(self.x - other.x, self.y - other.y, self.z - other.z)
-        
         return NotImplemented
         
     def __rsub__(self, other):
         if isinstance(other, (int, float)):
             return Vec3(other - self.x, other - self.y, other - self.z)
-            
         return NotImplemented
     
+    def __mul__(self, other):
+        if isinstance(other, (int, float)):
+            return Vec3(self.x * other, self.y * other, self.z * other)
+        return self.dot(other)
+    
+    def __rmul__(self, other):
+        if isinstance(other, (int, float)):
+            return Vec3(self.x * other, self.y * other, self.z * other)
+        return NotImplemented
+    
+    def __truediv__(self, other):
+        if isinstance(other, (int, float)):
+            return Vec3(self.x / other, self.y / other, self.z / other)
+        return NotImplemented
+    
+    def __rtruediv__(self, other):
+        if isinstance(other, (int, float)):
+            return Vec3(other / self.x, other / self.y, other / self.z)
+        return NotImplemented
+    
+    def __matmul__(self, other):
+        if isinstance(other, 'Vec3'):
+            return Vec3(self.x * other.x, self.y * other.y, self.z * other.z)
+        return NotImplemented
+    
+    def __rmatmul__(self, other):
+        return NotImplemented
+
     def __str__(self):
         return f"({self.x} ; {self.y} ; {self.z})"
+    
+    def dot(self, other: 'Vec3'):
+        if isinstance(other, Vec3):
+            return self.x*other.x + self.y*other.y + self.z*other.z
+
+        return NotImplemented
 
     def norm(self):
         return (self.x*self.x + self.y*self.y + self.z*self.z) ** 0.5
@@ -59,4 +89,17 @@ class Vec3():
         return Vec3(self.x / norm, self.y / norm, self.z / norm)
 
 a = Vec3(5, 10, 15)
-print(a)
+b = Vec3(-2, -5, -10)
+assert a
+
+# Addition
+assert 1+a
+assert a+1
+assert a+b
+assert b+a
+
+# Substraction
+assert 1-a
+assert a-1
+assert a-b
+assert b-a
